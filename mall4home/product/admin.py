90dd2f12a2133d70298b218fd7afe502f5c7a71f
adarsh <admadarsh500@gmail.com>
2022-06-07 11:40:15 +0530

    7 1) product app ne therichu backup chethu (git merge chethu) 2) product app delete akkunathinu munbe product/admin nill pro_stroe set chethu admin pannel ill product add chethu 3)index pageill for loop koduthu name,price access chethu 4)mall4home/setting media_url ,root set chethu 5)mall4home/url set akki 6)image import chethu 7)user click chethunna athe product details konduvannu 8)product ne backup chethathu shesham comment section html careathe chethu (product.html) 9)product/models comment class creathe chethu 10) migartion chethilla

--- mall4home/product/admin.py
+++ mall4home/product/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import pro_store
+
+admin.site.register(pro_store)
 
 # Register your models here.
--- mall4home/product/models.py
+++ mall4home/product/models.py
@@ -1,4 +1,5 @@
 from distutils.command.upload import upload
+from itertools import product
 from django.db import models
 
 # Create your models here.
@@ -7,4 +8,13 @@ class pro_store(models.Model):
     p_price=models.FloatField()
     p_image=models.ImageField(upload_to ='pic')
     p_desc=models.TextField()
+    date=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.p_name
+    
+class comment(models.Model):
+    product=models.ForeignKey(pro_store,related_name="comments",on_delete=models.CASCADE)
+    u_name=models.CharField(max_length=100)
+    body=models.TextField()
     date=models.DateTimeField(auto_now_add=True)
